6860b9c2b1cc48d7e63b03fc1b40efa92342e48c
Craig <craig.fox.jd@icloud.com>
2016-04-08 13:14:30 +1200

    Initial Angular 2 starting point

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import RootComponent from 'app/root/app-root';
+
+bootstrap(RootComponent);
---
+++ app/root/app-root.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app/root/app-root.html'
+})
+export default class RootComponent {}
