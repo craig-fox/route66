abe5105aa35e65054adc96db432d9d5cef883769
Craig <craig.fox.jd@icloud.com>
2016-04-12 12:51:01 +1200

    Fixes

--- app/root/app-root.ts
+++ app/root/app-root.ts
@@ -7,15 +7,14 @@ import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 @Component({
     selector: 'app-root',
     templateUrl: 'app/root/app-root.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, HomeComponent, MapComponent, LocationDetailComponent]
 })
 
 @RouteConfig(
     [
         {path: '/', component:HomeComponent, as: 'Home'},
         {path: '/map', component:MapComponent, as: 'Map'},
-        {path: '/location', component:LocationDetailComponent, as: 'LocationDetail'},
-       
+        {path: '/location', component:LocationDetailComponent, as: 'LocationDetail'}
     ]
 )
 
